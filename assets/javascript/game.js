431919e038cd8192340f81682fc4fef29854ec5e
Todd Croak-Falen <Todd Croak-Falen>
2016-07-24 13:15:37 -0700

    Added pseudo code and images.

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$( document ).ready(){
+
+}
+
+// The computer will display a random number BETWEEN 19 AND 120 -- see the random number generator we made in class.
+// Each of the four crystals has A HIDDEN RANDOM VALUE BETWEEN 1 AND 12.
+// Run the function.
+// When a player clicks on a crystal button, it will add a specific amount of points to the player's total score.
+// If the player's score matches the random number.
+// If the player's score goes higher than the random number, they lose the game.
+// Total score is displayed and updated after each click -- BUT THE VALUE OF EACH CRYSTAL IS NOT DISPLAYED.
+// Update number of games won and lost.  Scoring system:
+// Matching the random number = 1 win.
+// If player's score goes higher than the random number = 1 loss.
+// Win or lose, the game is, "reset," which will run the function at the beginning all over again.
+// When reset:
+// New random number...
+// All crystals have four different hidden values...
+// Player's total score is reset to zero -- except that in the video demo, the score is carried over.
